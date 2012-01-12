be7369c86696e363ce10a882ba1aa9dafc160823
Haofu Han <Haofu.Han.973@gmail.com>
2012-01-12 22:47:57 +0800

    merge code from yueafrozan
    destroy Notification after click on 'STOP'
    blablabla...

---
+++ src/com/Lab973/GreenSmartphone/CPULogger.java
@@ -0,0 +1,77 @@
+package com.Lab973.GreenSmartphone;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.RandomAccessFile;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class CPULogger extends MyLogger{
+	String log="";
+	public CPULogger(String logFileName, int interval) {
+		super(logFileName, interval);
+	}
+
+	@Override
+	public String getLogValue() {
+		// TODO Auto-generated method stub
+		double cpuused=0;
+		String[] toks=null;
+		
+		try 
+		{ 
+			RandomAccessFile reader = new RandomAccessFile("/proc/stat", "r");			   
+		
+			String load = reader.readLine();			   
+			toks = load.split(" ");			   
+			long idle1 = Long.parseLong(toks[5]);			   
+			long cpu1 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[4]) +Long.parseLong(toks[5])+ Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);   
+			
+			Thread.sleep(this.logInterval);   
+			reader.seek(0);
+			   
+			load = reader.readLine();  
+			reader.close();			   
+			toks = load.split(" ");			   
+			long idle2 = Long.parseLong(toks[5]);		   
+			long cpu2 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[4]) + Long.parseLong(toks[5])+Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);			   
+			cpuused=100*((cpu2-cpu1 )-(idle2-idle1))/ (cpu2-cpu1 );
+			return cpuused+"%";
+			} catch (IOException ex) { ex.printStackTrace();} catch (InterruptedException e) {
+				e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			this.stopFlag = true;
+		    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/sdcard/cpu.txt",false)));
+		    SimpleDateFormat bartDateFormat =  
+		    		  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");  
+			while(stopFlag){
+				long timeInMillis = System.currentTimeMillis();
+				Calendar cal = Calendar.getInstance();
+				cal.setTimeInMillis(timeInMillis);
+				Date date = cal.getTime();
+				log+=bartDateFormat.format(date)+"\t"+ this.getLogValue() + "\r\n";
+			}
+			if(!stopFlag)
+			{
+				out.write(log);
+				out.close();
+			}
+		} catch (IOException e) {//open file failed!
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/com/Lab973/GreenSmartphone/LogService.java
@@ -0,0 +1,82 @@
+package com.Lab973.GreenSmartphone;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+public class LogService extends Service {
+	//CpuThread ct;
+	int interval=1;
+	boolean screenchecked=false;
+	boolean cpuchecked=false;
+	boolean memorychecked=false;
+	boolean packetchecked=false;
+	CPULogger cl;
+	ScreenLogger sl;
+	NotificationManager mNM;
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		 
+		mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+		showNotification();
+		super.onCreate();
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		if(cpuchecked)cl.stopLog();
+		if(screenchecked)sl.stopLog();
+		destroyNotification();
+		super.onDestroy();
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		// TODO Auto-generated method stub
+		interval=intent.getIntExtra("Interval", 1000);
+		cpuchecked=intent.getBooleanExtra("Cpu", false);
+		screenchecked=intent.getBooleanExtra("Screen", false);
+		
+		if(cpuchecked)
+		{
+			cl=new CPULogger("/sdcard/cpu.txt", interval);	
+			cl.start();
+		}
+		if(screenchecked)
+		{
+			sl=new ScreenLogger("/sdcard/screen.txt", interval, getApplicationContext());	
+			sl.start();
+		}
+		super.onStart(intent, startId);
+	}
+	private void showNotification() {
+        Notification notification = new Notification(R.drawable.ic_launcher, "AndroidLogger is Logging...",
+                System.currentTimeMillis());
+        Intent intent = new Intent(getApplicationContext(),MobleEventCollectorActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+        		getApplicationContext(), 0, intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setLatestEventInfo(this, "AndroidLogger",
+        		"Logging...", pendingIntent);
+        mNM.notify("NOTIFICATION",0, notification);
+    }
+    private void destroyNotification() {
+        mNM.cancel("NOTIFICATION", 0);
+    }
+
+
+}
